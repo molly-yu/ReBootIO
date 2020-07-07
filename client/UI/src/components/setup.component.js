298320fdfd0dab2b61abc566cdacacbdc03ec014
Molly Yu <m234yu@uwaterloo.ca>
2020-07-07 14:49:38 -0400

    Added checkbox component

--- client/UI/src/components/setup.component.js
+++ client/UI/src/components/setup.component.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
 import axios from 'axios';
 import styled from 'styled-components';
+import Checkbox from "./checkbox.component";
 const Styles = styled.div`
 text-align:left;
 background: white;
@@ -12,13 +13,24 @@ width:100%;
  z-index: 5;
 `;
 
-
 export default class Setup extends Component{
+    state = { checked: false }
+
+    handleCheckboxChange = event => {
+        this.setState({ checked: event.target.checked })
+    }
+
     render(){
         return(
             <Styles>
             <div className="Setup" id="setup">
                 <h2>Reboot:</h2>
+                <label>
+                <Checkbox
+                checked={this.state.checked}
+                onChange={this.handleCheckboxChange}/>
+                <span style={{marginLeft:8}}>Label Text</span>
+                </label>
             </div>
             </Styles>
         )
